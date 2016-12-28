ab8e695793820ce27d27e9a75565b2808f41377c
suciokastro <suciokastro@gmail.com>
2016-12-28 10:47:22 -0500

    change allow origin header

--- httpserver.go
+++ httpserver.go
@@ -7,10 +7,21 @@ import (
 )
 
 var port = flag.String("port", "8080", "Define what TCP port to bind to")
-var root = flag.String("root", ".", "Define the root filesystem path")
+var root = flag.String("root", "/sdcard", "Define the root filesystem path")
 
 func main() {
 	flag.Parse()
 	log.Println("Starting web server at http://0.0.0.0:" + *port)
-	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*root))))
+
+	changeHeader := func(h http.Handler) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("Access-Control-Allow-Origin", "*")
+			w.Header().Add("Access-Control-Allow-Credentials", "true")
+			w.Header().Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			h.ServeHTTP(w, r)
+		}
+	}
+
+	http.Handle("/", changeHeader(http.FileServer(http.Dir(*root))))
+	panic(http.ListenAndServe(":"+*port, nil))
 }
